c551fda476bcf8b30b96c9776b4d6ac2a1831707
marcus <linfaimom@gmail.com>
2016-08-29 15:58:02 +0800

    界面优化

---
+++ app/src/main/java/com/marcus/wechatnews/ui/custom/CustomView.java
@@ -0,0 +1,21 @@
+package com.marcus.wechatnews.ui.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by marcus on 16/8/25.
+ */
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
